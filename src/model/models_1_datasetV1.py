50d9e4cbd2cc30dda0b14627cbd9579a3d0f516e
Mike Pearmain <michael.pearmain@gmail.com>
2017-05-31 18:22:30 +0100

    end to end run

--- src/model/models_1_datasetV1.py
+++ src/model/models_1_datasetV1.py
@@ -1,7 +1,7 @@
 import fastparquet
 import pandas as pd  # data processing, CSV file I/O (e.g. pd.read_csv)
 from sklearn.linear_model import LinearRegression
-from sklearn.ensemble import RandomForestRegressor, ExtraTreesRegressor
+from sklearn.ensemble import RandomForestRegressor
 from gestalt.stackers.stacking import GeneralisedStacking
 from gestalt.estimator_wrappers.wrap_xgb import XGBRegressor
 from sklearn.metrics import r2_score
@@ -11,7 +11,7 @@ pd.options.display.max_columns = 999
 
 # Fix the folds - generates skf object
 from sklearn.model_selection import KFold
-skf = KFold(shuffle=True, n_splits=10, random_state=260681)
+skf = KFold(shuffle=True, n_splits=20, random_state=260681)
 
 """
 build_datasetV1 has had no treatment to the object or count columns and so is unsuitable for any models other than 
@@ -42,13 +42,17 @@ params1 = {"eta": 0.05, "subsample": 0.8, "min_child_weight": 1, "colsample_bytr
            "max_depth": 8, 'silent': 1, "n_jobs": 12}
 params2 = {"eta": 0.025, "subsample": 0.7, "min_child_weight": 3, "colsample_bytree": 0.7,
            "max_depth": 10, 'silent': 1, "n_jobs": 12}
+params3 = {"eta": 0.01, "subsample": 0.9, "min_child_weight": 2, "colsample_bytree": 0.9, "gamma":0.5,
+           "max_depth": 4, 'silent': 1, "n_jobs": 12}
 
-estimators = {RandomForestRegressor(n_estimators=500, n_jobs=12, random_state=42): 'RFR1' + BUILD_NAME,
-              ExtraTreesRegressor(n_estimators=500, n_jobs=12, random_state=42): 'ETR1' + BUILD_NAME,
+
+estimators = {RandomForestRegressor(n_estimators=100, n_jobs=12, random_state=42, max_features=0.8): 'RFR1' + BUILD_NAME,
               XGBRegressor(num_round=5000, verbose_eval=False, params=params1, early_stopping_rounds=25,
                            eval_metric=xgb_r2_score): 'XGB1' + BUILD_NAME,
               XGBRegressor(num_round=5000, verbose_eval=False, params=params2, early_stopping_rounds=25,
-                           eval_metric=xgb_r2_score): 'XGB2' + BUILD_NAME}
+                           eval_metric=xgb_r2_score): 'XGB2' + BUILD_NAME,
+              XGBRegressor(num_round=5000, verbose_eval=False, params=params3, early_stopping_rounds=50,
+                           eval_metric=xgb_r2_score): 'XGB3' + BUILD_NAME}
 
 merc = GeneralisedStacking(base_estimators_dict=estimators,
                            estimator_type='regression',
